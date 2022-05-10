d85f12c97e25670450ed3aff4e899ca853a03262
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2022-05-10 23:14:40 +0200

    add some audios

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 dr((((((o))))))ne jam
 
 https://itch.io/jam/drone
+
+Built with A-Frame: https://aframe.io/
+
+Textures: https://www.solarsystemscope.com/textures/
+
+Sounds:
+* Sun: NASA | Sun Sonification (raw audio) https://www.youtube.com/watch?v=-I-zdmg_Dno
+* Mercury: The European Space Agency | Sound of the solar wind at Mercury https://www.esa.int/Science_Exploration/Space_Science/BepiColombo/BepiColombo_s_first_tastes_of_Mercury_science
+* Venus: NASA Goddard | NASA's Parker Solar Probe Discovers Natural Radio Emission in Venus' Atmosphere https://www.youtube.com/watch?v=z5vK6-wuoOE
+* Earth: NASA/University of Iowa | Chorus Plasma Waves | Whistler Waves | Plasmawaves - Chorus | Plasmaspheric Hiss https://www.nasa.gov/feature/goddard/2017/nasa-listens-in-as-electrons-whistle-while-they-work
+* Mars: NASA | First Likely Marsquake Heard By NASA's InSight | "Dinks and donks" from InSight's seismometer https://soundcloud.com/nasa/first-likely-marsquake-heard-by-nasas-insight?in=nasa/sets/spookyspacesounds & https://www.nasa.gov/connect/sounds/index.html
--- play-on-click.js
+++ play-on-click.js
@@ -2,15 +2,35 @@ AFRAME.registerComponent('play-on-click', {
     init: function () {
         this.el.addEventListener('click', () => {
             const start = document.querySelector('#start');
+            const startText = document.querySelector('#startText');
             start.setAttribute('visible', 'false');
-            // var context = new AudioContext();
-            // context.resume().then(() => {
-            //     console.log('playing')
-            //     const sun = document.querySelector('#sunSound');
-            //     const merc = document.querySelector('#mercSound');
-            //     sun.components.sound.playSound();
-            //     merc.components.sound.playSound();
-            // })
+            startText.setAttribute('visible', 'false');
+            var context = new AudioContext();
+            context.resume().then(() => {
+                console.log('playing')
+                const sun1 = document.querySelector('#sunSound1');
+                const sun2 = document.querySelector('#sunSound2');
+                const sun3 = document.querySelector('#sunSound3');
+                const merc = document.querySelector('#mercSound');
+                const venus = document.querySelector('#venusSound');
+                const earth = document.querySelector('#earthSound');
+                const earth2 = document.querySelector('#earthSound2');
+                const earth3 = document.querySelector('#earthSound3');
+                const earth4 = document.querySelector('#earthSound4');
+                const mars = document.querySelector('#marsSound');
+                const mars2 = document.querySelector('#marsSound2');
+                sun1.components.sound.playSound();
+                sun2.components.sound.playSound();
+                sun3.components.sound.playSound();
+                merc.components.sound.playSound();
+                venus.components.sound.playSound();
+                earth.components.sound.playSound();
+                earth2.components.sound.playSound();
+                earth3.components.sound.playSound();
+                earth4.components.sound.playSound();
+                mars.components.sound.playSound();
+                mars2.components.sound.playSound();
+            })
         })
     }
 });
